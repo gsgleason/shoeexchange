3f06c28689fb4b9c0f2382bbdadabc4182ce4f5f
Gregory Gleason <gsgleason@gmail.com>
2020-11-02 14:10:47 -0700

    defined username and password from config once

--- extend.py
+++ extend.py
@@ -2,6 +2,9 @@ import requests
 from config import config
 from lxml import etree
 
+username = config['pythonanywhere']['username']
+password = config['pythonanywhere']['password']
+
 #create session to share cookies and socket
 s = requests.Session()
 
@@ -17,8 +20,8 @@ token = doc.xpath("//input[@name='csrfmiddlewaretoken']")[0].attrib['value']
 
 # data to be posted
 payload = {
-        'auth-username': config['pythonanywhere']['username'],
-        'auth-password': config['pythonanywhere']['password'],
+        'auth-username': username,
+        'auth-password': password,
         'csrfmiddlewaretoken': token,
         'login_view-current_step': 'auth'
         }
@@ -33,32 +36,32 @@ r = s.post(url, data=payload, )
 if r.history:
     s.headers.update({'referer': 'https://www.pythonanywhere.com{}'.format(r.history[-1].headers.get('Location'))})
 
-url = 'https://www.pythonanywhere.com/user/{0}/webapps/#tab_id_{0}_pythonanywhere_com'.format(config['pythonanywhere']['username'])
-
+# load the webapp tab
+url = 'https://www.pythonanywhere.com/user/{0}/webapps/#tab_id_{0}_pythonanywhere_com'.format(username)
 r = s.get(url)
 doc = etree.HTML(r.text)
 
-token = doc.xpath("//form[@action='/user/{0}/webapps/{0}.pythonanywhere.com/extend']/input[@name='csrfmiddlewaretoken']".format(config['pythonanywhere']['username']))[0].attrib['value']
+# find the token
+token = doc.xpath("//form[@action='/user/{0}/webapps/{0}.pythonanywhere.com/extend']/input[@name='csrfmiddlewaretoken']".format(username))[0].attrib['value']
 
+#set referer header
+s.headers.update({'referer': 'https://www.pythonanywhere.com/user/{}/webapps/'.format(username)})
 
-s.headers.update({'referer': 'https://www.pythonanywhere.com/user/{}/webapps/'.format(config['pythonanywhere']['username'])})
-
+#post to extend web app
 payload = {
         'csrfmiddlewaretoken': token
         }
-
-url = "https://www.pythonanywhere.com/user/{0}/webapps/{0}.pythonanywhere.com/extend".format(config['pythonanywhere']['username'])
-
+url = "https://www.pythonanywhere.com/user/{0}/webapps/{0}.pythonanywhere.com/extend".format(username)
 r = s.post(url, data=payload)
 
 # GET the tasks tab
-url = "https://www.pythonanywhere.com/user/{}/tasks_tab/".format(config['pythonanywhere']['username'])
+url = "https://www.pythonanywhere.com/user/{}/tasks_tab/".format(username)
 r = s.get(url)
 s.headers.update({'referer': url})
 token = doc.xpath("//input[@name='csrfmiddlewaretoken']")[0].attrib['value']
 
 #get schedule
-url = "https://www.pythonanywhere.com/api/v0/user/shoeexchange/schedule/".format(config['pythonanywhere']['username'])
+url = "https://www.pythonanywhere.com/api/v0/user/shoeexchange/schedule/".format(username)
 r = s.get(url)
 schedule = r.json()
 
